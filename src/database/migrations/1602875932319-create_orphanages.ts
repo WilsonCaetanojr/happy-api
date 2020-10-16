a9a9dd0172121df7fdbe43b8443ab257f5b41ae4
Wilson Caetano <wilsonjr_caetano@hotmail.com>
2020-10-16 16:35:39 -0300

    Realiza correções, e roda a migration da tablea orphanages

--- src/database/migrations/1602875932319-create_orphanages.ts
+++ src/database/migrations/1602875932319-create_orphanages.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class createOrphanages1602627668658 implements MigrationInterface {
+export class createOrphanages1602875932319 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
@@ -9,8 +9,8 @@ export class createOrphanages1602627668658 implements MigrationInterface {
           {
             name: "id",
             type: "integer",
-            unsigned: true,
             isPrimary: true,
+            unsigned: true,
             isGenerated: true,
             generationStrategy: "increment"
           },
