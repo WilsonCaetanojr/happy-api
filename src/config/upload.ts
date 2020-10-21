ea75998a81dedd05adf1b50f121abcfa2a46228e
Wilson Caetano <wilsonjr_caetano@hotmail.com>
2020-10-21 17:30:47 -0300

    Adiciona novas configurações no multer para uploads de imagens

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,13 +1,23 @@
-import multer from "multer";
+import multer, { Options } from "multer";
 import path from "path";
 
 export default {
   storage: multer.diskStorage({
     destination: path.join(__dirname, "..", "..", "uploads"),
-    filename: (req, file, callback) => {
-      const fileName = `${Date.now()}-${file.originalname}`;
-
-      callback(null, fileName);
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}-${file.originalname}`);
     }
-  })
-};
+  }),
+  limits: {
+    fileSize: 4 * 1024 * 1024 // 4MB
+  },
+  fileFilter: (req, file, cb) => {
+    const mimeTypes = ["image/jpeg", "image/png"];
+
+    if (!mimeTypes.includes(file.mimetype)) {
+      return cb(null, false);
+    }
+
+    cb(null, true);
+  }
+} as Options;
